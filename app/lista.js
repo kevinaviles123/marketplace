c18de11b0d19332cfbedaa991e0fb9aeb76fb3f3
kevinaviles123 <Kevinstevengananaviles@gmail.com>
2024-08-09 16:40:54 -0500

    quinto commit

---
+++ app/lista.js
@@ -0,0 +1,107 @@
+const products = [
+    {
+        name: "courvoisier",
+        image: "Asstes/imagenes/21 botellas.png",
+        price: 93160165.38
+    },
+    {
+        name: "Remy martin",
+        image: "Asstes/imagenes/Botella 1.png",
+        price: 8274332.47
+    },
+    {
+        name: "martell",
+        image: "Asstes/imagenes/Botella dos.jpg",
+        price: 8610861.05
+    },
+    {
+        name: "Louis Xlll",
+        image: "Asstes/imagenes/Botella 3.jpg",
+        price: 27127620.30
+    },
+    {
+        name: "Carlos lll",
+        image: "Asstes/imagenes/Cuarta botella.jpg",
+        price: 120000.00
+    },
+    {
+        name: "REMY MARTI",
+        image: "Asstes/imagenes/Quinto botella.jpg",
+        price: 14313568.50
+    },
+    {
+        name: "ARMAGNAC SEMPE",
+        image: "Asstes/imagenes/Sexto botella.jpg",
+        price: 13132131.10
+    },
+    {
+        name: "Hennessy",
+        image: "Asstes/imagenes/Séptima botella.png",
+        price: 21783888.06
+    },
+    {
+        name: "REMY MARTIN",
+        image: "Asstes/imagenes/Octavo botella.png",
+        price: 12990000.00
+    },
+    {
+        name: "Hennessy X.O",
+        image: "Asstes/imagenes/Novena botella.jpg",
+        price: 5043828.90
+    },
+    {
+        name: "LARSEN V.S.O.P",
+        image: "Asstes/imagenes/Décima botella.jpg",
+        price: 41436644.81
+    },
+    {
+        name: "XANTE LIQUEOR",
+        image: "Asstes/imagenes/11 botellas.jpg",
+        price: 112841236.14
+    },
+    {
+        name: "CARLOS I DESDE 1889",
+        image: "Asstes/imagenes/12 botellas.jpg",
+        price: 21463501.50
+    },
+    {
+        name: "RICHARD HENNESS",
+        image: "Asstes/imagenes/13 botellas.jpg",
+        price: 21776936.76
+    },
+    {
+        name: "COMTE JOSEPH",
+        image: "Asstes/imagenes/14 botellas.gif",
+        price: 19246741.98
+    },
+    {
+        name: "COMTE JOSEPH",
+        image: "Asstes/imagenes/15 botellas.jpg",
+        price: 1300000.00
+    },
+    {
+        name: "ZACAPA",
+        image: "Asstes/imagenes/27 botellas.jpg",
+        price: 15224477.00
+    },
+    {
+        name: "Hennessy",
+        image: "Asstes/imagenes/20 botellas.png",
+        price: 4680958.60
+    },
+    {
+        name: "Mistial",
+        image: "Asstes/imagenes/26 botellas.jpg",
+        price: 15291939.65
+    },
+    {
+        name: "Hennessy",
+        image: "Asstes/imagenes/23 botellas.jpeg",
+        price: 147277500.34
+    },
+    {
+        name: "CANNABIS ABSINTHE",
+        image: "Asstes/imagenes/25 botellas.jpg",
+        price: 13685145.18
+    }
+];
